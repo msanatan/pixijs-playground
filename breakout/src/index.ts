4969e4ec53093cb6f883e0d0599a0db84f4d435c
Marcus Sanatan <msanatan@gmail.com>
2020-09-24 23:57:44 -0400

    Load custom fonts + set up HUD
    Simple HUD shows title, lives and score

--- breakout/src/index.ts
+++ breakout/src/index.ts
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import * as WebFont from 'webfontloader';
 
 const canvas = <HTMLCanvasElement>document.getElementById('app');
 
@@ -11,4 +12,67 @@ const app = new PIXI.Application({
   autoDensity: true,
   antialias: true,
 });
+
+const webFontConfig: WebFont.Config = {
+  custom: {
+    families: ['Hikou Outline', 'Hikou Light'],
+    urls: ['css/fonts.css']
+  },
+  active() {
+    init();
+  },
+};
+
+WebFont.load(webFontConfig);
+
+const renderHUD = (app: PIXI.Application, lives: number) => {
+  const hudContainer: PIXI.Container = new PIXI.Container();
+  const titleText = new PIXI.Text('Breakout', {
+    fontFamily: 'Hikou Outline',
+    fontSize: 100,
+    fill: 'white',
+    align: 'center',
+  });
+  titleText.anchor.set(0.5);
+  titleText.position.set(app.renderer.screen.width / 2, 50);
+
+  // Write HUD info
+  const livesText = new PIXI.Text('Lives', {
+    fontFamily: 'Hikou Light',
+    fontSize: 20,
+    fill: 'white',
+    align: 'left',
+  });
+  livesText.anchor.set(0.5);
+  livesText.position.set(35, 160);
+
+  // TODO: draw heart sprites
+
+  const scoreText = new PIXI.Text('Score', {
+    fontFamily: 'Hikou Light',
+    fontSize: 20,
+    fill: 'white',
+    align: 'left',
+  });
+  scoreText.anchor.set(0.5);
+  scoreText.position.set(app.renderer.screen.width / 2, 160);
+
+  // Draw horizontal line below HUD info
+  const graphics = new PIXI.Graphics();
+  graphics.lineStyle(2, 0xffffff, 1);
+  graphics.moveTo(0, 180);
+  graphics.lineTo(app.renderer.screen.width, 180);
+  graphics.closePath();
+  graphics.endFill();
+
+  hudContainer.addChild(titleText);
+  hudContainer.addChild(livesText);
+  hudContainer.addChild(scoreText);
+  hudContainer.addChild(graphics);
+  app.stage.addChild(hudContainer);
+};
+
+function init() {
+  renderHUD(app, 3);
+}
 
