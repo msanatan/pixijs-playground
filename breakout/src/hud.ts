acf0a9b2f3474888a48e521dba0e85db8f2d2477
Marcus Sanatan <msanatan@gmail.com>
2020-09-27 19:38:38 -0400

    Refactor HUD into a class
    Like game, we'll eventually need a way to update the HUD

--- breakout/src/hud.ts
+++ breakout/src/hud.ts
@@ -1,48 +1,54 @@
 import * as PIXI from 'pixi.js';
 
-export const renderHUD = (app: PIXI.Application, lives: number) => {
-  const hudContainer: PIXI.Container = new PIXI.Container();
-  const titleText = new PIXI.Text('Breakout', {
-    fontFamily: 'Hikou Outline',
-    fontSize: 100,
-    fill: 'white',
-    align: 'center',
-  });
-  titleText.anchor.set(0.5);
-  titleText.position.set(app.renderer.screen.width / 2, 50);
+export default class HUD {
+  private container: PIXI.Container;
+  constructor() {
+    this.container = new PIXI.Container();
+  }
 
-  // Write HUD info
-  const livesText = new PIXI.Text('Lives', {
-    fontFamily: 'Hikou Light',
-    fontSize: 20,
-    fill: 'white',
-    align: 'left',
-  });
-  livesText.anchor.set(0.5);
-  livesText.position.set(35, 160);
+  init(app: PIXI.Application, lives: number, score: number) {
+    const titleText = new PIXI.Text('Breakout', {
+      fontFamily: 'Hikou Outline',
+      fontSize: 100,
+      fill: 'white',
+      align: 'center',
+    });
+    titleText.anchor.set(0.5);
+    titleText.position.set(app.renderer.screen.width / 2, 50);
 
-  // TODO: draw heart sprites
+    // Write HUD info
+    const livesText = new PIXI.Text('Lives', {
+      fontFamily: 'Hikou Light',
+      fontSize: 20,
+      fill: 'white',
+      align: 'left',
+    });
+    livesText.anchor.set(0.5);
+    livesText.position.set(35, 160);
 
-  const scoreText = new PIXI.Text('Score', {
-    fontFamily: 'Hikou Light',
-    fontSize: 20,
-    fill: 'white',
-    align: 'left',
-  });
-  scoreText.anchor.set(0.5);
-  scoreText.position.set(app.renderer.screen.width / 2, 160);
+    // TODO: draw heart sprites
 
-  // Draw horizontal line below HUD info
-  const graphics = new PIXI.Graphics();
-  graphics.lineStyle(2, 0xffffff, 1);
-  graphics.moveTo(0, 180);
-  graphics.lineTo(app.renderer.screen.width, 180);
-  graphics.closePath();
-  graphics.endFill();
+    const scoreText = new PIXI.Text(`Score: ${score}`, {
+      fontFamily: 'Hikou Light',
+      fontSize: 20,
+      fill: 'white',
+      align: 'left',
+    });
+    scoreText.anchor.set(0.5);
+    scoreText.position.set(app.renderer.screen.width / 2, 160);
 
-  hudContainer.addChild(titleText);
-  hudContainer.addChild(livesText);
-  hudContainer.addChild(scoreText);
-  hudContainer.addChild(graphics);
-  app.stage.addChild(hudContainer);
-};
+    // Draw horizontal line below HUD info
+    const graphics = new PIXI.Graphics();
+    graphics.lineStyle(2, 0xffffff, 1);
+    graphics.moveTo(0, 180);
+    graphics.lineTo(app.renderer.screen.width, 180);
+    graphics.closePath();
+    graphics.endFill();
+
+    this.container.addChild(titleText);
+    this.container.addChild(livesText);
+    this.container.addChild(scoreText);
+    this.container.addChild(graphics);
+    app.stage.addChild(this.container);
+  }
+}
